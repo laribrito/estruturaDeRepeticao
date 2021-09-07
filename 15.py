9797ac5638e843e7e2a552a241923e9bb3fce76c
laribrito <lary.29.ds@gmail.com>
2021-09-07 09:16:27 -0300

    Insere resoluçao - exercicio 15 parte 1
    O exercicio 15 esta respondido, mas vou trabalhar na exibiçao dele,
    visando a estetica do programa.

---
+++ 15.py
@@ -0,0 +1,21 @@
+#coding: UTF-8
+import os
+
+def limpar(): 
+	os.system("clear" or "cls")
+
+#QUESTÃO: A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,... 
+# Faça um programa capaz de gerar a série até o n−ésimo termo.
+
+limpar()
+print(" - Sequência de Fibonacci -")
+cont = int(input("Número 'n' de termos: ")) #recebe o contador do usuário
+x = 0
+a = 1 #primeiro termo da sequência
+b = 0 #termo anterior
+while x<cont:
+    print(a) 
+    c = a #armazena o termo anterior em uma variável auxiliar
+    a += b #soma o atual com o anterior
+    b = c #b recebe o termo anterior
+    x+=1
